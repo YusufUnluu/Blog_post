3e7ab910d0fab5069b648a31b8152732dd5749f5
YusufUnluu <yunlu.TR.Ru@gmail.com>
2024-03-27 00:03:02 +0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import IndexScreen from './src/screens/IndexScreen';
+import CreateScreen from './src/screens/CreateScreen';
+import { Provider } from './context/BlogContext';
+import ShowScreen from './src/screens/ShowScreen';
+import { AntDesign } from '@expo/vector-icons';
+import EditScreen from './src/screens/EditScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerTitle:"Blog"}}>
+          <Stack.Screen name="Index" component={IndexScreen} options={({navigation}) =>({
+            headerRight:() =>(
+              <TouchableOpacity onPress={() => {
+                navigation.navigate("Create")
+              }}>
+                <AntDesign name="plus" size={24} color="black" />
+              </TouchableOpacity>
+            )
+          })} />
+          <Stack.Screen name="Create" component={CreateScreen} />
+          <Stack.Screen name="Show" component={ShowScreen} options={({navigation,route}) =>({
+            headerRight:() =>(
+              <TouchableOpacity onPress={() => {
+                navigation.navigate("Edit",{id:route.params.id})
+              }}>
+                <AntDesign name="edit" size={35} color="black" />
+              </TouchableOpacity>
+            )
+          })}/>
+          <Stack.Screen name="Edit" component={EditScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({})
---
+++ api/jsonServer.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+// NGROK DAN ÇEKMEYİ UNUTMA
+export default axios.create({
+    baseURL:""
+})
